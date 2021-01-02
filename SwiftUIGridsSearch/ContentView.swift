581a05fd5c7254ee018cedea8a88a9d694c61dc0
monkeyidesign <sam@monkeyidesign.com>
2021-01-02 10:08:52 +0700

    Create grid ui

--- SwiftUIGridsSearch/ContentView.swift
+++ SwiftUIGridsSearch/ContentView.swift
@@ -9,8 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView{
+                LazyVGrid(columns: [
+                    //GridItem(.fixed(100)),
+                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12),
+                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12),
+                    GridItem(.flexible(minimum: 100, maximum: 200))
+                ], spacing:12, content:{
+                    
+                    ForEach(0..<20, id: \.self) { num in
+//                        HStack {
+//                            Spacer()
+//                            Text("\(num)")
+//                            Spacer()
+//                        }
+//                        .padding()
+//                        .background(Color.red)
+                        VStack(alignment: .leading, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+                            Spacer()
+                                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                                .background(Color.blue)
+                            
+                            Text("App Title")
+                                .font(.system(size: 10, weight: .semibold))
+                            Text("Release Date")
+                                .font(.system(size: 9, weight: .regular))
+                            Text("Copyright")
+                                .font(.system(size: 9, weight: .regular))
+                                .foregroundColor(.white)
+                        })
+                        .padding()
+                        .background(Color.red)
+                    }
+                }).padding(.horizontal, 12)
+            }.navigationTitle("Grid Search")
+        }
+       
     }
 }
 
