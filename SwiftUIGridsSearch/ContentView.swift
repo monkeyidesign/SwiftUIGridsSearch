384e20a7ddb94ad9862ec62892f69f5646d85b9a
monkeyidesign <sam@monkeyidesign.com>
2021-01-02 09:15:11 +0700

    Initial Commit

---
+++ SwiftUIGridsSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIGridsSearch
+//
+//  Created by Apple on 1/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIGridsSearch/SwiftUIGridsSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGridsSearchApp.swift
+//  SwiftUIGridsSearch
+//
+//  Created by Apple on 1/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGridsSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
